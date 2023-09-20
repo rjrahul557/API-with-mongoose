fc4c15c7580f76076ad2c4017ddd6de3d84710d4
Rahul <rahulrjz600@gmail.com>
2023-09-20 17:44:18 +0530

    second commit

--- index.js
+++ index.js
@@ -7,35 +7,24 @@ const app = express();
 app.use(express.json());
 
 
-app.post('/create',async(req,res)=>{
-    let data = new product(req.body);
-    let result = await data.save();
-    console.log(result);
-    res.send(result);
-})
 
 
-app.get('/list',async(req,res)=>{
-    let data = await product.find();
+
+app.get('/search/:key',async(req,res)=>{
+    let data = await product.find(
+        {
+            "$or":[
+                {name : {$regex:req.params.key}}
+            ]
+        }
+    );
     
     console.log(data);
     res.send(data);
 })
 
-app.delete('/delete/:_id',async(req,res)=>{
-    console.log(req.params);
-    let data = await product.deleteOne(req.params);
-    res.send(data);
-})
 
-app.put('/update/:_id',async(req,res)=>{
-    console.log(req.params);
-    let data = await product.updateOne(
-        req.params,
-        {
-            $set:req.body
-        }
-    );
-    res.send(data);
-})
+
+
+
 app.listen(5000);
